29cdc09269d2a6239b15861d8a7b7b09460ba71e
ruhul amin <mail.ruhul005@gmail.com>
2018-04-05 23:06:52 +0600

    sql file updated
    newviewww sql updated

--- newviewww.sql
+++ newviewww.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 15, 2018 at 04:49 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.0.15
+-- Host: localhost
+-- Generation Time: Apr 05, 2018 at 07:04 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -54,7 +56,10 @@ INSERT INTO `advertise` (`id`, `advertise_name`, `user_id`) VALUES
 (16, 'dhasjk', 4),
 (17, 'dnkas', 4),
 (18, 'kol', 4),
-(19, 'wow bd', 9);
+(19, 'wow bd', 9),
+(20, '50 $ per day', 10),
+(21, '4$ A DAY', 11),
+(22, 'i am Super man', 12);
 
 -- --------------------------------------------------------
 
@@ -71,10 +76,10 @@ CREATE TABLE `user` (
   `email` varchar(30) NOT NULL,
   `password` varchar(50) NOT NULL,
   `phone` int(30) NOT NULL,
-  `NID` varchar(100) NOT NULL,
-  `bkash` int(20) NOT NULL,
-  `rocket` int(20) NOT NULL,
-  `bank` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `skill` varchar(100) NOT NULL,
+  `cgpa` varchar(4) NOT NULL,
+  `current_status` varchar(50) NOT NULL,
   `educational_status` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -82,15 +87,18 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `image`, `firstname`, `lastname`, `username`, `email`, `password`, `phone`, `NID`, `bkash`, `rocket`, `bank`, `educational_status`) VALUES
-(1, '6907050-fish-windows-7-wallpaper.jpg', 'joy', 'hossain', 'joy12', 'joy@gmail.com', '1234', 12345, '3983', 7313846, 373847, '864381964', 'ssc,hsc,higher'),
-(3, 'diu.jpg', 'jok', 'kio', 'nayem', 'nayem@gmail.com', '1234', 12345, '', 0, 0, '', 'ssc,hsc,higher'),
-(4, 'wallpaper_for_windows_7.jpg', 'joy', 'hossain', 'jot12', 'jot@gmail.com', '1234', 95423, '12374', 9231, 2391208, 'djjd', 'hsc'),
-(5, '', '', '', '', '', '', 0, '', 0, 0, '', ''),
-(6, 'Koala.jpg', 'jok', 'hojj', 'sdjnv', 'djnvj@gmail.com', '12344', 3948938, '', 0, 0, '', ''),
-(7, 'Koala.jpg', 'jok', 'hojj', 'sdjnvhj', 'djnvjhh@gmail.com', '12345', 3948938, '', 0, 0, '', ''),
-(8, 'Desert.jpg', 'hot', 'pot', 'pot12', 'pot@gmail.com', '12345', 987665544, '', 0, 0, '', ''),
-(9, 'Tulips.jpg', 'ert', 'rtt', 'rtt', 't@gmail.com', '1234', 12234454, '', 0, 0, '', '');
+INSERT INTO `user` (`id`, `image`, `firstname`, `lastname`, `username`, `email`, `password`, `phone`, `address`, `skill`, `cgpa`, `current_status`, `educational_status`) VALUES
+(1, '6907050-fish-windows-7-wallpaper.jpg', 'joy', 'hossain', 'joy12', 'joy@gmail.com', '1234', 12345, '3983', '7313846', '3738', '864381964', 'ssc,hsc,higher'),
+(3, 'diu.jpg', 'jok', 'kio', 'nayem', 'nayem@gmail.com', '1234', 12345, '', '0', '0', '', 'ssc,hsc,higher'),
+(4, 'wallpaper_for_windows_7.jpg', 'joy', 'hossain', 'jot12', 'jot@gmail.com', '1234', 95423, '12374', '9231', '2391', 'djjd', 'hsc'),
+(5, '', '', '', '', '', '', 0, '', '0', '0', '', ''),
+(6, 'Koala.jpg', 'jok', 'hojj', 'sdjnv', 'djnvj@gmail.com', '12344', 3948938, '', '0', '0', '', ''),
+(7, 'Koala.jpg', 'jok', 'hojj', 'sdjnvhj', 'djnvjhh@gmail.com', '12345', 3948938, '', '0', '0', '', ''),
+(8, 'Desert.jpg', 'hot', 'pot', 'pot12', 'pot@gmail.com', '12345', 987665544, '', '0', '0', '', ''),
+(9, 'Tulips.jpg', 'ert', 'rtt', 'rtt', 't@gmail.com', '1234', 12234454, '', '0', '0', '', ''),
+(10, 'Advertise.png', 'Ruhul', 'Amin', 'rrrr', '123@gmail.com', '123456', 1234567890, '', '0', '0', '', ''),
+(11, 'platform.jpg', 'KAMAL', 'SAIDI', 'SSS', '1234@gmail.com', '112233', 1243567, '', '', '', '', ''),
+(12, '', 'ash', 'ilam', 'ruhulamin', '12345@gmail.com', '12345', 2345, 'dhaka', 'superman', '4.00', 'employeed', 'null');
 
 --
 -- Indexes for dumped tables
@@ -117,12 +125,14 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `advertise`
 --
 ALTER TABLE `advertise`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- Constraints for dumped tables
 --
@@ -132,6 +142,7 @@ ALTER TABLE `user`
 --
 ALTER TABLE `advertise`
   ADD CONSTRAINT `advertise_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
